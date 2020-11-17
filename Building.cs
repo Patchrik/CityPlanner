5f7c67e186db7a0742badab27cfd71392c225c71
Patchrik <pklly92@gmail.com>
2020-11-17 13:18:23 -0600

    Finished with part 1 of the urban planner.

---
+++ Building.cs
@@ -0,0 +1,59 @@
+using System;
+
+namespace Planner
+{
+  // Define class here
+  public class Building
+  {
+    // private fields
+    private string _designer { get; } = "Parker Kelley";
+    private DateTime _dateConstructed { get; set; }
+    private string _address { get; set; }
+    private string _owner { get; set; }
+
+    // Public Properties
+    public int Stories { get; set; }
+    public double Width { get; set; }
+    public double Depth { get; set; }
+    public double Volume
+    {
+      get
+      {
+        return Width * Depth * (3 * Stories);
+      }
+    }
+
+    // Constructor for the Building class/Object
+    public Building(string address)
+    {
+      if (address.Length > 5)
+      {
+        _address = address;
+      }
+    }
+
+    // Construct method will set the private _dateConstructed field
+    public void Construct()
+    {
+      _dateConstructed = DateTime.Now;
+    }
+    // Purchase method will set the private _owner string in the Building object
+    public void Purchase(string buyerName)
+    {
+      _owner = buyerName;
+    }
+
+    // PrintDeed will write out the console 
+    public void PrintDeed()
+    {
+      Console.WriteLine("");
+      Console.WriteLine($"{_address}");
+      Console.WriteLine("---------------");
+      Console.WriteLine($"Designed by {_designer}");
+      Console.WriteLine($"Constructed on {_dateConstructed}");
+      Console.WriteLine($"Owned by {_owner}");
+      Console.WriteLine($"{Volume} cubic meters of space");
+    }
+
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
 
-namespace CityPlanner
+namespace Planner
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Building Overton = new Building("4960 Overton Brentwood");
+
+      Building EightSixFive = new Building("865 Bellvue Rd");
+
+      Building SpoopVille = new Building("666 Tragic Way");
+
+      Overton.Width = 1000;
+      Overton.Depth = 650;
+      Overton.Stories = 2;
+
+      Overton.Construct();
+
+      Overton.Purchase("Doug Dimmadome");
+
+
+
+      EightSixFive.Width = 2500;
+      EightSixFive.Depth = 1000;
+      EightSixFive.Stories = 2;
+
+      EightSixFive.Construct();
+
+      EightSixFive.Purchase("Social Leech");
+
+      SpoopVille.Width = 5000;
+      SpoopVille.Depth = 666.13;
+      SpoopVille.Stories = 13;
+
+      SpoopVille.Construct();
+
+      SpoopVille.Purchase("Lucifer Def-Not Thatwon");
+
+
+      Console.WriteLine("Hello City Planner!");
+      Console.WriteLine("-------------------");
+
+      Overton.PrintDeed();
+
+      EightSixFive.PrintDeed();
+
+      SpoopVille.PrintDeed();
+
+
+
     }
+  }
 }
